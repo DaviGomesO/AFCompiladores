8dd79cab8326bea634caef83dd85689f78182e9e
Davi Gomes <davigomes@alu.ufc.br>
2023-03-30 19:53:12 -0300

    Update

--- At1.c
+++ At1.c
@@ -2,58 +2,101 @@
 #include <stdlib.h>
 #include <string.h>
 
-int ler_Cadeia(int estadoAtual, char elementoAtualCadeia){
-    if(estadoAtual == 0){
-        if(elementoAtualCadeia == '0'){
+int ler_Cadeia(int estadoAtual, char elementoAtualCadeia)
+{
+    if (estadoAtual == 0)
+    {
+        if (elementoAtualCadeia == '0')
+        {
             return 0;
-        }else if(elementoAtualCadeia == '1'){
+        }
+        else if (elementoAtualCadeia == '1')
+        {
             return 1;
         }
-    }else if(estadoAtual == 1){
-        if(elementoAtualCadeia == '0'){
+    }
+    else if (estadoAtual == 1)
+    {
+        if (elementoAtualCadeia == '0')
+        {
             return 1;
-        }else if(elementoAtualCadeia == '1'){
+        }
+        else if (elementoAtualCadeia == '1')
+        {
             return 2;
         }
-    }else if(estadoAtual == 2){
-        if(elementoAtualCadeia == '0' || elementoAtualCadeia == '1'){
+    }
+    else if (estadoAtual == 2)
+    {
+        if (elementoAtualCadeia == '0' || elementoAtualCadeia == '1')
+        {
             return 3;
         }
     }
 }
 
-int main()
+// Automato
+int estadoInicial = 0, estadoAceitacao = 2;
+
+void Final(int estadoAtual)
 {
-    char *cadeia;
-    cadeia = (char*)malloc(sizeof(char) * 80);
+    if (estadoAtual == estadoAceitacao)
+    {
+        printf("A cadeia pertence ao AFND.");
+    }
+    else
+    {
+        printf("A cadeia nao pertence ao AFND.");
+    }
+}
 
-    printf("Digite a cadeia: ");
-    scanf("%s",cadeia);
-
-    //tamanho da cadeia
-    int tamCadeia = strlen(cadeia);
-
-    //declara o estado inicial do automato
-    int estadoAtual = 0;
+void Automato(char *cadeia, int tamCadeia)
+{
+    int estadoAtual = estadoInicial;
 
     int i, contPadrao = 0;
-    for(i = 0; i < tamCadeia; i++){
-        estadoAtual = ler_Cadeia(estadoAtual, cadeia[i]);
+    for (i = 0; i < tamCadeia; i++)
+    {
+        if (cadeia[i] == '0' || cadeia[i] == '1')
+        {
+            estadoAtual = ler_Cadeia(estadoAtual, cadeia[i]);
+        }
 
-        if(estadoAtual == 3){
+        // aqui ele vai ter passado pelo estado de aceitação, e ainda tem caracteres para continuar lendo
+        if (estadoAtual == 3)
+        {
             contPadrao++;
             estadoAtual = 0;
+            // garante que vai continuar a ler a cadeia a partir do elemento que levou ao q3
             i--;
         }
 
-        if(i == tamCadeia-1 && estadoAtual == 2){
+        // verifica se está no fim da cadeia e se esstá no estado de aceitação
+        if (i == tamCadeia - 1 && estadoAtual == 2)
+        {
             contPadrao++;
         }
     }
 
-    //verificar se está em um estado de aceitação
+    // verificar se está em um estado de aceitação
+    Final(estadoAtual);
 
-    printf("\nEstado ATUAL: %d\nForam encontrados %d padroes.", estadoAtual, contPadrao);
+    printf("\nEstado ATUAL: %d\nPADROES encontrados: %d.", estadoAtual, contPadrao);
+}
+
+int main()
+{
+    char *cadeia;
+    cadeia = (char *)malloc(sizeof(char) * 80);
+
+    printf("Digite a cadeia: ");
+    scanf("%s", cadeia);
+
+    // tamanho da cadeia
+    int tamCadeia = strlen(cadeia);
+
+    // declara o estado inicial do automato
+    Automato(cadeia, tamCadeia);
 
     return 0;
 }
